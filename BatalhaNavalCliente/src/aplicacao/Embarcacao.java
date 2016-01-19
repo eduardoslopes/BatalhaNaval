a08a0c8ee94ad78b3ca865a10864c851f3e3f8c1
Murilo <j.murilo.o.jr@gmail.com>
2016-01-19 13:35:35 -0200

    Criada a interface Embarcação.

---
+++ BatalhaNavalCliente/src/aplicacao/Embarcacao.java
@@ -0,0 +1,15 @@
+package aplicacao;
+
+public interface Embarcacao {
+	
+	/* Retorna true sse todas as células que compõem a embarcação estiverem afundadas.
+	 * Caso contrário retorna false. */
+	public boolean afundada ();
+	/* Retorna a posição na linha do tabuleiro em que se encontra a primeira célula da
+	 * embarcação. */
+	public int posX ();
+	/* Retorna a posição da coluna do tabuleiro em que se encontra a primeira célula da
+	 * embarcação. */
+	public int posY ();
+
+}
