bc4c68a349af9b8b199ce951eee4c85188631548
julioserafim <serafimjuliom583@gmail.com>
2016-01-21 19:07:54 -0300

    "Atualizacao"

--- BatalhaNavalCliente/src/aplicacao/ControladorJogo.java
+++ BatalhaNavalCliente/src/aplicacao/ControladorJogo.java
@@ -2,24 +2,10 @@ package aplicacao;
 
 public class ControladorJogo {
 	
-	public boolean jogoAcabou(){
-		return false;	
+	public void enviarMensagem(){
+		
 	}
 	
-	public boolean usuarioDesconectado(){
-		return false;
-	}
 	
-	public boolean tiroAgua(int posx, int posy){
-		return false;
-	}
-	
-	public boolean tiroEmbacacao(){
-		return false;
-	}
-	
-	public boolean embarcacaoFoiDestruida(){
-		return false;
-	}
 	
 }
