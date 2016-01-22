4d80155a792ecbc85daf2d5a2e23527e1653992f
julioserafim <serafimjuliom583@gmail.com>
2016-01-22 12:16:49 -0300

    "Concretes Factories"

--- BatalhaNavalCliente/src/aplicacao/model/TAG.java
+++ BatalhaNavalCliente/src/aplicacao/model/TAG.java
@@ -4,4 +4,6 @@ public class TAG {
 	public static String CONECTG = "CONECTG";
 	public static String DESCONECTG = "DESCONECTG";
 	public static String MOVE = "MOVE";
+	public static String CREATEG = "CREATEG";
+	public static String SEEG = "SEEG";
 }
