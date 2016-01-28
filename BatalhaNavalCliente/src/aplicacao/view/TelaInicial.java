e4a215ca29496885e18f43e00f0d88ef37c8ddc4
wandersoncs <wandersoncorreiasouza@gmail.com>
2016-01-28 13:36:22 -0300

    Tela Inicial

--- BatalhaNavalCliente/src/aplicacao/view/TelaInicial.java
+++ BatalhaNavalCliente/src/aplicacao/view/TelaInicial.java
@@ -6,10 +6,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-/**
- * @author Wanderson
- *
- */
 public class TelaInicial extends Application {
 
 	private static Stage stage;
